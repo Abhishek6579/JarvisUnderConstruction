e312428cf02d64f9f5b2a181a24b7bdc9db12022
Abhishek6579 <83967453+Abhishek6579@users.noreply.github.com>
2021-12-25 12:28:58 +0530

    Update WhatsAppChatSpammer.py

--- WhatsAppChatSpammer.py
+++ WhatsAppChatSpammer.py
@@ -12,5 +12,5 @@ def spam(text, noOFMessages=10):
 if __name__ == "__main__":
     text = input("Enter the text to spam : ")
     noOFMessages = int(input("Enter the no of times you want to send the message : "))
-    sleep(4)
+    sleep(4) # Use this time to open whatsapp and the person whom you want to msg, the cursor should be palced on the message box.
     spam(text, noOFMessages)
